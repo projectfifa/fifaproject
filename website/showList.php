4003536dd2943f7df6c0d1ec1b018a42ddb00bf5
BuildTools <unconfigured@null.spigotmc.org>
2017-05-09 15:09:49 +0200

    ShowList Header
    Showlist style update,
    now fully supports all arrays,
    column headers added

--- website/showList.php
+++ website/showList.php
@@ -7,9 +7,18 @@ class showList {
 		$arrayKeys = array_keys($list[0]);
 		$columnCnt = count($list[0]);
 
+
+		// Header
 		echo '<div class="showListHeader">'; echo $header; echo'</div>				
 		      <div class="showListContent">';
 
+		echo '<div class="row-list">';
+		for ($i=1; $i < $columnCnt; $i++) { 
+ 				echo '<div>'.$arrayKeys[$i].'</div>';
+ 			}
+ 			echo '</div>';
+
+		// Rows
 		foreach ($list as $row) {
  			echo '<div class="row-list">';
 
@@ -19,9 +28,67 @@ class showList {
  			echo '</div>';
 		}
 
+		// Footer
 		echo '</div>
 		      <div class="showListFooter"></div>';
 
+
+
+		/*					VOORBEELD OUTPUT HTML*/
+		echo '
+		<div class="showListHeader"> Title </div>
+		<div class="showListContent">
+			<div class="row-list">
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+			</div>
+			<div class="row-list">
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+			</div>
+			<div class="row-list">
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+			</div>
+			<div class="row-list">
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+			</div>
+			<div class="row-list">
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+			</div>
+			<div class="row-list">
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+				<div> item1 </div>
+				<div> item2 </div>
+				<div> item3 </div>
+			</div>
+		</div>
+		<div class="showListFooter"></div>';
+		
 	}
 }
 ?>
