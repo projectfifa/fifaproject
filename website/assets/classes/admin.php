3605ff7facf9595394c533a597f9f9f80faf9d68
Unknown <bp153373@edu.rocwb.nl>
2017-05-11 12:19:58 +0200

    admin class function add

--- website/assets/classes/admin.php
+++ website/assets/classes/admin.php
@@ -12,7 +12,7 @@ class admin
     {
         try
         {
-            $sql = "INSERT INTO `tbl_admins` (team_id_a, team_id_b) VALUES (:ida, :idb)";
+            $sql = "INSERT INTO `tbl_matches` (team_id_a, team_id_b) VALUES (:ida, :idb)";
             $stmt = $this->db->prepare($sql);
             $stmt->bindparam(":ida", $team_id_a);
             $stmt->bindparam(":idb", $team_id_b);
@@ -30,7 +30,7 @@ class admin
     {
         try
         {
-            $sql = "INSERT INTO `tbl_admins` (score_team_a, score_team_b) VALUES (:scorea, :scoreb)";
+            $sql = "INSERT INTO `tbl_matches` (score_team_a, score_team_b) VALUES (:scorea, :scoreb)";
             $stmt = $this->db->prepare($sql);
             $stmt->bindparam(":scorea", $score_team_a);
             $stmt->bindparam(":scoreb", $score_team_b);
@@ -44,6 +44,23 @@ class admin
         }
     }
 
-    
+    public function setPlayer($first_name, $last_name, $student_id)
+    {
+        try
+        {
+            $sql = "INSERT INTO `tbl_players` (student_id, first_name, last_name) VALUES (:id, :firstname, :lastname)";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindparam(":id", $student_id);
+            $stmt->bindparam(":firstname", $first_name);
+            $stmt->bindparam(":lastname", $last_name);
+            $stmt->execute();
+
+            return $stmt;
+        }
+        catch(PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+    }
 
 }
